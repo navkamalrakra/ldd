781c5d6c07f5863004bcf1a65211dddf29cdad07
Navkamal Rakra <navkamal90@gmail.com>
2013-11-30 23:56:18 +0000

    Simple Dynamic allocation of Major and Minor number

---
+++ major_minor/major_minor.c
@@ -0,0 +1,28 @@
+#include<linux/module.h>
+#include<linux/version.h>
+#include<linux/types.h>
+#include<linux/kdev_t.h>
+#include<linux/fs.h>
+
+static dev_t num;
+
+static int entry_module(void)
+{
+	if (alloc_chrdev_region(&num, 0, 3, "Navkamal Rakra") < 0)
+	{
+		return -1;
+	}
+	printk(KERN_INFO "Registered with %d as Major and %d as Minor\n",MAJOR(num),MINOR(num));
+	return 0;
+}
+
+static void bye_module(void)
+{
+	unregister_chrdev_region(num,3);
+	printk(KERN_INFO "Unregistered successfully, Good to Go\n");
+}
+
+module_init(entry_module);
+module_exit(bye_module);
+
+MODULE_LICENSE("GPL");
